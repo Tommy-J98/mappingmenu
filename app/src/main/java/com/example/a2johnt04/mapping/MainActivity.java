ea6966068e813d8306c83bfa0fdbc5d1665590b9
tommy-j98 <2johnt04@solent.ac.uk>
2018-02-01 12:52:14 +0000

    01/02/2018 added text long and lat input

--- app/src/main/java/com/example/a2johnt04/mapping/MainActivity.java
+++ app/src/main/java/com/example/a2johnt04/mapping/MainActivity.java
@@ -11,12 +11,17 @@ import android.view.MenuItem;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
-import org.osmdroid.views.MapView;
+import org.osmdroid.views.MapView;import android.view.View.OnClickListener;import android.view.View;import android.widget.TextView;
+import android.widget.EditText;import android.widget.Button;import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity
+
+public class MainActivity extends AppCompatActivity implements OnClickListener
 {
 
     MapView mv;
+
+
+
 
     /** Called when the activity is first created. */
     @Override
@@ -35,5 +40,24 @@ public class MainActivity extends AppCompatActivity
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(16);
         mv.getController().setCenter(new GeoPoint(42.05,1.72));
+        Button b = (Button)findViewById(R.id.btn1);
+        b.setOnClickListener(this);
+
     }
+
+        public void onClick(View view)
+        {
+        EditText longtxt = (EditText) findViewById(R.id.longitude);
+        EditText lattxt = (EditText)findViewById(R.id.latitude);
+        double longitude = Double.parseDouble(lattxt.getText().toString());
+        double latitude = Double.parseDouble(longtxt.getText().toString());
+            mv.getController().setCenter(new GeoPoint(latitude,longitude));
+        }
 }
+
+
+
+
+
+
+
